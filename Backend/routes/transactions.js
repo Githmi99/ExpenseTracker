42b68c9ca40c56702375cf58caeecce209ece511
githmi99 <amarathuga@gmail.com>
2024-03-18 22:36:30 +0530

    updated

--- Backend/routes/transactions.js
+++ Backend/routes/transactions.js
@@ -1,1 +1,11 @@
-const router = require('express').Router()
+const { addIncome, getIncomes, deleteIncome } = require('../controllers/income');
+
+const router = require('express').Router();
+
+
+
+router.post('/add-income', addIncome)
+    .get('/get-incomes', getIncomes)
+    .delete('/delete-income/:id', deleteIncome)
+
+module.exports = router
